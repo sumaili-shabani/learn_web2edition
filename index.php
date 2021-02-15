29bd3e272a34e6772f020e3c92e67eef0ea2c003
sumaili-shabani <sumailiroger681@gmail.com>
2021-02-15 10:52:50 +0100

    initialisation de l'introduction bootstrap v4 premier

--- index.php
+++ index.php
@@ -2,10 +2,16 @@
 <html>
 <head>
 	<title>bienvenue chez chez devtech 2em édition!!!</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="viewport" content="width=-devicewidth, initial-scale=1, shrink-to-fit=no">
 
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
+	<!-- <link rel="stylesheet" type="text/css" href="assets/css/bootstrap/css/bootstrap.min.css"> -->
+
+	<link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.min.css">
+
+	<!-- recuperation script en ligne -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<!-- fin recuperation script en ligne -->
 
 	<link rel="shortcut Icon" type="text/css" href="assets/images/logo_default.jpg">
 	<meta name="robots" content="follow, all">
@@ -17,113 +23,154 @@
 </head>
 <body>
 
-<header>
-	<nav id="bloc_menu">
-			<div id="menu_link">
-				<a href="index.php" style="color: white;">Accueil</a> &nbsp;|&nbsp;
+<!-- insertion de menu principal -->
+<nav class="navbar navbar-expand-lg navbar-white bg-dark">
+  
 
-				<a href="blog.php" style="color: white;">notre bloc Blog</a>&nbsp;|&nbsp;
-				<a href="contact.php" style="color: white;">Contact</a>&nbsp;|&nbsp;
-				<a href="javascript:void(0);" style="color: white;">Service</a>
-			</div>
-	</nav>
-</header>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent2">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><i class="fa fa-cc-visa"></i>&nbsp;Nous faire un don <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="tel:+243817883541"><i class="fa fa-phone"></i>&nbsp; +243817883541</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="mailto:info.devtechedition2@gmail.com"> <i class="fa fa-envelope"></i>&nbsp; info.devtechedition2@gmail.com</a>
+      </li>
+     
+     
+    </ul>
+    
+  </div>
+</nav>
+<!-- insertion de menu principal -->
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: rgb(225, 84, 34);">
+  <a class="navbar-brand" href="#" class="img-responsive">
+  	<img src="assets/images/carousel/bootrap ok.png" class="img img-thumbnail img-responsive" style="width: 60px; height: 30px;">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><i class="fa fa-home"></i>&nbsp; Accueil <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fa fa-tag"></i>&nbsp; Blog</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fa fa-globe"></i>&nbsp;Contact</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-cog"></i>&nbsp; Paramètre
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#"><i class="fa fa-cc-mastercard"></i>&nbsp; Nous faire un don</a>
+          <a class="dropdown-item" href="#"><i class="fa fa-eyedropper"></i>&nbsp; Condition d'utilisation du site</a>
+          <div class="dropdown-divider"></div>
+           <a class="dropdown-item" href="#"><i class="fa fa-picture-o"></i>&nbsp; Nos galeries</a>
+          <a class="dropdown-item" href="#"><i class="fa fa-video-camera"></i>&nbsp; Nos vidéos</a>
+        </div>
+      </li>
+     
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+     <input type="search" name="search" id="seach" placeholder="Rechercher" class="form-control mr-sm-2">
+     <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fa fa-search"></i>&nbsp; Recherche</button>
+    </form>
+  </div>
+</nav>
+<!-- insertion de menu principal -->
 
-
-<p>
-<h1 align="center">Accueil</h1>
-
-<div align="center">
-
-	<div>
-
-		Les développeurs Devtech visent à construire l'avenir en éduquant et en résolvant les problèmes en utilisant la TECHNOLOGIE.
-		
-	</div>
-
-	<p>
-		suivez notre charmante formation en css et html debidant pour nous comprendre
-	</p>
-	
-
-	LES audios et vidéos
-	suivez lac doube reggue <br>
-	<audio src="assets/audio/LUCK DUBE Raegga. .MP3" controls></audio> 
-	<br><br>
-	suivez cours d'ajax <br>
-	<video src="./assets/video/2.webm" poster="./assets/images/logo 1.jpg"  controls>
-		<source type="video/mp4" src="./assets/video/2.webm">
-		<source type="video/webm" src="./assets/video/2.webm">
-	</video>
-
-	<video src="./assets/video/1.mp4" poster="./assets/images/logo_default.jpg"  controls>
-		<source type="video/mp4" src="./assets/video/1.mp4">
-		<source type="video/webm" src="./assets/video/1.mp4">
-	</video>
-	<br><br>
-
-
-
+<!-- insertion de carousel -->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="assets/images/carousel/bootrap ok.png" alt="First slide" style="height: 400px;">
+      <div class="carousel-caption">
+      	<p>
+      		<h3>Explorez des formations au prix le plus bas jamais atteint!</h3>
+      		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
+      		<p>
+      			<button class="btn btn-primary"><i class="fa fa-facebook"></i> Facebook</button>
+      			<button class="btn btn-danger"><i class="fa fa-google"></i> Google+</button>
+      		</p>
+      		.
+      	</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/images/carousel/php.png" alt="Second slide" style="height: 400px;">
+      <div class="carousel-caption">
+      	<p>
+      		<h3>Introduction de l'image carousel!</h3>
+      		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
+      		<p>
+      			<a href="https://github.com/sumaili-shabani/" target="_blank" class="btn btn-secondary"><i class="fa fa-github"></i> Github</a>
+      			<a href="javascript:void(0);" class="btn btn-danger"><i class="fa fa-google"></i> Linkdin</a>
+      		</p>
+      		.
+      	</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/images/carousel/jquery.jpg" alt="Third slide" style="height: 400px;">
+       <div class="carousel-caption">
+      	<p>
+      		<h3>Suivez plus nos formations en programmation web niveau 1</h3>
+      		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
+      		
+      		.
+      	</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Precedent</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Suivant</span>
+  </a>
 </div>
+<!-- fin insertion carousel -->
 
-<footer>
-
-	
-
-	<div class="img_center" align="center">
-		<p>
-			<img src="assets/images/user-icon.jpg" class="img_default_user"> 
-			<div class="text_default_description_ceo">
-				CEO: isaac muhyana
-			Développeur et entreprenneur. au paravant developpeur et administrqteur gestionnaire des bases des données chez <a href="javascript:void(0);">DATABANK</a><font class="ref_drc">rdc</font>
-			</div>
-		</p>
-	</div>
-	<div class="alignement" align="center">
-		<ul>Ressource
-			<li>
-				<a href="index.php">Accueil</a>
-			</li>
-			<li>
-				<a href="index.php">Blog</a>
-			</li>
-			<li>
-				<a href="index.php">Projet</a>
-			</li>
-		</ul>
-
-		<ul>Contact pour information
-			<li>
-				<a href="index.php">Contact</a>
-			</li>
-			<li>
-				<a href="index.php">Service</a>
-			</li>
-			<li>
-				<a href="index.php">Mission</a>
-			</li>
-		</ul>
-
-		<ul>Prennez contact avec nous!
-			<li>
-				<a href="mailto:usaacmuhyana@gmail.com">usaacmuhyana@gmail.com</a>
-			</li>
-			<li>
-				<a href="tel:+243978150288">+243978150288</a>
-			</li>
-			<li>
-				<a href="javascript:void(0);">Goma drc quartier mbangasub tmk</a>
-			</li>
-		</ul>
+	<div class="container mb-2" style="margin-top: 10px;">
+		<h4 class="text-center">Introduction bootstrap!</h4>
 	</div>
 
 
 
-	Copyright @ 2020 Développeurs Devtech . Tous les droits sont réservés
-	
-</footer>
+<!-- script hors connexion	 -->
+<!-- <script type="text/javascript" src="assets/js/jquery-3.2.1.slim.min.js"></script>
+<script type="text/javascript" src="assets/js/popper.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script> -->
+<!-- fin script hors connexion	 -->
 
+<!-- recuperation script en ligne -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<!-- fin recuperation script en ligne -->
+
+<script type="text/javascript">
+	$(document).ready(function() {
+		
+		$('.carousel').carousel({
+		  interval: 3000
+		})
+	});
+</script>
 
 </body>
 </html>
